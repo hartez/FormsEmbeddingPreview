5f911a45cb1104c65a427447ef20f13b7fff10a1
E.Z. Hart <hartez@gmail.com>
2017-09-19 13:29:44 -0600

    Add iOS project

---
+++ FormsEmbeddingPreview.iOS/AppDelegate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Foundation;
+using TipCalc;
+using UIKit;
+using Xamarin.Forms;
+
+namespace FormsEmbeddingPreview.iOS
+{
+	// The UIApplicationDelegate for the application. This class is responsible for launching the
+	// User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+	[Register("AppDelegate")]
+	public class AppDelegate : UIApplicationDelegate
+	{
+		public static AppDelegate Shared;
+		public static UIStoryboard Storyboard = UIStoryboard.FromName("Main", null);
+
+		UIViewController _tipCalc;
+		UIWindow _window;
+		UINavigationController _navigation;
+		ViewController _mainController;
+
+		public override UIWindow Window
+		{
+			get;
+			set;
+		}
+
+		public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+		{
+			// Before the first time we use it, we need to initialize Xamarin.Forms
+			Forms.Init();
+
+			Shared = this;
+			_window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+			_mainController = Storyboard.InstantiateInitialViewController() as ViewController;
+			_navigation = new UINavigationController(_mainController);
+
+			_window.RootViewController = _navigation;
+			_window.MakeKeyAndVisible();
+
+			return true;
+		}
+
+		public void NavigateToTipCalc()
+		{
+			if (_tipCalc == null)
+			{
+				_tipCalc = new TipCalcPage().CreateViewController();
+			}
+
+			_navigation.PushViewController(_tipCalc, true);
+		}
+	}
+}
---
+++ FormsEmbeddingPreview.iOS/ViewController.cs
@@ -0,0 +1,19 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace FormsEmbeddingPreview.iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+	    public override void ViewDidLoad()
+	    {
+		    base.ViewDidLoad();
+		    NavigateToTipCalculator.TouchUpInside += (sender, e) => AppDelegate.Shared.NavigateToTipCalc();
+	    }
+    }
+}
