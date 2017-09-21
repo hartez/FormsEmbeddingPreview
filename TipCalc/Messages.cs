1d068f2d8616fc990f91ce1291b80984b4939ef2
E.Z. Hart <hartez@gmail.com>
2017-09-21 16:55:17 -0600

    Update to package from myget

---
+++ TipCalc/Messages.cs
@@ -0,0 +1,30 @@
+﻿namespace TipCalc
+{
+	public static class Messages
+	{
+		public static object Sender = new object();
+
+		public const string InitialAmount = "InitialAmount";
+		public const string Tip = "Tip";
+	}
+
+	public class InitialAmountArgs
+	{
+		public InitialAmountArgs(double initialAmount)
+		{
+			InitialAmount = initialAmount;
+		}
+
+		public double InitialAmount { get; }
+	}
+
+	public class TipArgs
+	{
+		public TipArgs(double tip)
+		{
+			Tip = tip;
+		}
+
+		public double Tip { get; }
+	}
+}
--- TipCalc/TipCalcModel.cs
+++ TipCalc/TipCalcModel.cs
@@ -4,34 +4,6 @@ using Xamarin.Forms;
 
 namespace TipCalc
 {
-	public static class Messages
-	{
-		public static object Sender = new object();
-
-		public const string InitialAmount = "InitialAmount";
-		public const string Tip = "Tip";
-	}
-
-	public class InitialAmountArgs
-	{
-		public InitialAmountArgs(double initialAmount)
-		{
-			InitialAmount = initialAmount;
-		}
-
-		public double InitialAmount { get; }
-	}
-
-	public class TipArgs
-	{
-		public TipArgs(double tip)
-		{
-			Tip = tip;
-		}
-
-		public double Tip { get; }
-	}
-
 	class TipCalcModel : INotifyPropertyChanged
 	{
 		double subTotal, postTaxTotal, tipPercent, tipAmount, total;
